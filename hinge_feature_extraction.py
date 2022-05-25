77a41c2a80a67d6cd094fb8e3e6f69b34f8be32c
osamamagdy <osamamagdy174@gmail.com>
2022-05-25 14:09:23 +0200

    hinge and cold

---
+++ hinge_feature_extraction.py
@@ -0,0 +1,42 @@
+import imutils
+from PIL import Image, ImageEnhance
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from utilities import *
+
+N_ANGLE_BINS = 40
+BIN_SIZE = 360 // N_ANGLE_BINS
+LEG_LENGTH = 25
+
+
+def get_hinge_features(contours):    
+    hist = np.zeros((N_ANGLE_BINS, N_ANGLE_BINS))
+        
+    # print([len(cnt) for cnt in contours])
+    for cnt in contours:
+        n_pixels = len(cnt)
+        if n_pixels <= LEG_LENGTH:
+            continue
+        
+        points = np.array([point[0] for point in cnt])
+        xs, ys = points[:, 0], points[:, 1]
+        point_1s = np.array([cnt[(i + LEG_LENGTH) % n_pixels][0] for i in range(n_pixels)])
+        point_2s = np.array([cnt[(i - LEG_LENGTH) % n_pixels][0] for i in range(n_pixels)])
+        x1s, y1s = point_1s[:, 0], point_1s[:, 1]
+        x2s, y2s = point_2s[:, 0], point_2s[:, 1]
+        
+        phi_1s = np.degrees(np.arctan2(y1s - ys, x1s - xs) + np.pi)
+        phi_2s = np.degrees(np.arctan2(y2s - ys, x2s - xs) + np.pi)
+        
+        indices = np.where(phi_2s > phi_1s)[0]
+        
+        for i in indices:
+            phi1 = int(phi_1s[i] // BIN_SIZE) % N_ANGLE_BINS
+            phi2 = int(phi_2s[i] // BIN_SIZE) % N_ANGLE_BINS
+            hist[phi1, phi2] += 1
+            
+    normalised_hist = hist / np.sum(hist)
+    feature_vector = normalised_hist[np.triu_indices_from(normalised_hist, k = 1)]
+    
+    return feature_vector
